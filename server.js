34f5f8e87ba1b5704d18b49330f0d51bf1b90165
Christopher osita <70627592+osita-dev@users.noreply.github.com>
2025-02-01 01:00:49 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ require("dotenv").config();
 const app = express();
 app.use(
   cors({
-    origin: 'https://peller-nation.vercel.app/',
+    origin: 'https://peller-nation.vercel.app',
     credentials: true,
   })
 );
